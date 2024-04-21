bd3937e7622c2802fe94d6a5de9f26787f4d4bf1
Guillermo <guillermoxr24@gmail.com>
2024-04-21 16:46:08 -0300

    Se comenzo la transformacion a DAO, hasta ahora funciona bien con mongo DB

--- src/repositories/products.repositories.js
+++ src/repositories/products.repositories.js
@@ -1,10 +1,14 @@
-import { ProductModel } from "../models/product.model.js";
+
+import { MongoProductsDAO } from "../dao/mongo.products.dao.js";
+
+
+const mongoProductsDAO = new MongoProductsDAO()
 
 export class ProductRepository{
    
     async getProducts(){
         try {
-            const products = await ProductModel.find()
+            const products = await mongoProductsDAO.getProducts()
             return products
         } catch (error) {
             throw new Error('Error al obtener productos...')
@@ -13,8 +17,7 @@ export class ProductRepository{
 
    async getProductById(productId){
          try {
-            const searchedProduct = await ProductModel.findById(productId)
-            if (!searchedProduct) return null
+            const searchedProduct = await mongoProductsDAO.getProductById(productId)
             return searchedProduct
         } catch (error) {
             throw new Error('Error al obtener producto...')
@@ -24,26 +27,17 @@ export class ProductRepository{
 
    async updateProduct(productId,newProduct){
     try {
-        const updatedProduct = await ProductModel.findByIdAndUpdate(productId, newProduct, { new: true });
-        if (!updatedProduct) {
-            console.log('Producto no encontrado...');
-            return {isSuccess: false,message: `No existe producto con id${productId}.`}
-        }
-        return {isSuccess: true,message: `Se edito el producto con id${productId}.`,updatedProduct}
+        const updatedProduct = await mongoProductsDAO.updateProduct(productId,newProduct)
+        return updatedProduct
     } catch (error) {
         throw new Error('Error al intentar actualizar producto...')
     }
    }
 
    async deleteProduct(productId){
-    console.log('ffgdsffssf')
-    try {
-        const deletedProduct = await ProductModel.findByIdAndDelete(productId) 
-        if (!deletedProduct){
-            console.log('Producto no encontrado...');
-            return {isSuccess: false,message: `No existe producto con id${productId}.`}
-        }
-        return {isSuccess: true,message: `Se elimino producto con id${productId}.`}
+   try {
+        const deletedResult = await mongoProductsDAO.deleteProduct(productId)
+        return deletedResult
         }  catch (error) {
         throw new Error('Error al intentar eliminar producto...')
     }
@@ -51,42 +45,19 @@ export class ProductRepository{
 
    async getProductsPaginate(limit,page,sort,query){
     try {
-      //sort puede ser 1,-1 o undefined
-      const sortBy = sort == 1 ? {price:1} : sort == -1 ? {price:-1} : {}
-      const filterBy = query ? query : {} //Provisorio hasta que se implemente el filtro visual
-      const products = await ProductModel.paginate(filterBy,{limit:limit,page:page, sort:sortBy})
-      return products
-
+        const products = await mongoProductsDAO(limit,page,sort,query)
+        return products
     } catch (error) {
       console.log('Error al recuperar los productos...')
       throw error
     }
   }
 
-      //Esta funcion agrega el producto y retorna el producto recien agregado. Retorna un objeto asi:
-    //{success: true/false, message: '',product: producto Agregado}
-    //Si producto no fue agregado devuelve null
+
     async addProduct({title, description,price,img,code,category,stock,status,thumbnails}){
         try{
-            //Comprobamos que vengan todos los campos en los parametros
-            if (!title || !description || !price|| !img || !code ||  !category || !stock || !status|| !thumbnails){
-                console.log('Es necesario ingresar todos los campos...')
-                return {success: false, message: 'Es necesario ingresar todos los campos...',product: null}
-            }
-            //Busco que el producto no exista.
-            console.log('Existe code: ', code)
-            const existProduct = await ProductModel.findOne({code:code})
-            console.log('Existe: ', existProduct)
-            if (existProduct){
-                //Si el codigo existe no agrego entonces salgo de la funcion enviando un mensaje a quien invoco
-                console.log('Existe un producto con este codigo...')
-                return {success: false, message:  `El producto no fue agregado. Ya existe un producto con codigo ${code}`,product: null}
-            }
-            //Si no existe procedemos a agregarlo.
-            const nuevoProducto = new ProductModel({title, description,price,img,code,category,stock,status,thumbnails})
-            nuevoProducto.save()
-            //guarde el producto en la base de Datos ahora mando msg de OK
-            return {success: true, message:`Se guardo en la BD el producto enviado bajo el id ${nuevoProducto.id}`,product:nuevoProducto}
+           const addResult = await mongoProductsDAO.addProduct({title, description,price,img,code,category,stock,status,thumbnails})
+           return addResult
         }catch(error){
             res.status(500)
         }
